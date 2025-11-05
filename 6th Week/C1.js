c41d2d1b35a7e58febbe8cc02dba4a4f640e2514
shohba0612 <adhikarishobha10@gmail.com>
2025-11-05 10:31:27 +0545

    Initial commit

---
+++ 6th Week/C1.js
@@ -0,0 +1,42 @@
+/*let x = "5"
+if (x==5){
+    console.log("inside if")
+}*/
+
+//and comparision operator
+
+/*let day = "sunday" //month = "September"
+if (day="sunday" && month=="September"){
+    console.log("The Class Day")
+} else{
+    console.log("The Class is Off.")
+} */
+
+/*let day = "Monday"//, month = "September"
+if (month=="September"|| day=="Monday"){
+    console.log("The Class Day")
+} else{
+    console.log("The Class is Off.")
+}*/
+
+/*let day = prompt ("Enter a Day: ")
+console.log('Day:', day)
+if (day=="Saturday"){
+    console.log("Holiday")
+} else if (day =="Sunday" || day=="Wednesday"){
+    console.log("WevDev Class")
+} else{
+    console.log("Other Class")
+}*/
+
+// n=2
+// for(let i=1; i<11; i++){
+//     output = "2*" + i + "=" + (i*n)
+//     console.log(output)
+// }
+
+n=2
+for(let i=1; i<11; i++){
+    output = (`${n} * ${i} = ${i*n}`)
+    console.log(output)
+}
---
+++ afterdash/C1.js
@@ -0,0 +1,16 @@
+// let x = 5;
+// let y = 5;
+// if (x>y){
+//     alert("x is greater then y.")
+// }else if(x==y){
+//     alert("X and Y are equal")
+// }else{
+//     alert("Not Greater")
+// }
+
+
+//Ternary Operator
+//(x>y)?console.log("greater"):console.log("Not greater") 
+
+let day="sunday";
+(day=="saturday")?alert("Today is holiday"):alert("today is not")
